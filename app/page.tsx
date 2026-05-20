16f049db48990eb861532f25959fa1436f6f58d3
Favour Ejiofor <faveejiofor2009@gmail.com>
2026-05-20 18:53:19 +0100

    fix: optimizing seo

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ import FooterScene from '@/components/scenes/FooterScene';
 import KeepScrolling from '@/components/ui/KeepScrolling';
 import Navbar from '@/components/ui/Navbar';
 import ContactModal from '@/components/ui/ContactModal';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main className="relative bg-black w-full min-h-screen">
+      <Head>
+        <link rel="icon" href="/favicon.jpeg" />
+      </Head>
       <Navbar />
       <TopScenes />
       <AboutScene />
